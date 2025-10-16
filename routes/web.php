bc8df916bfec2ca2885dbf3d7ea7affd8617867a
tahnia13 <tahnia24si@mahasiswa.pcr.ac.id>
2025-10-16 11:19:19 +0700

    crud

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\QuestionController;
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 
@@ -41,3 +42,5 @@ Route::post('question/store', [QuestionController::class, 'store'])
 
 Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
