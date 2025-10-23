111be572a30c87a8ad1fee2431c1446d3c6d0215
tahnia13 <tahnia24si@mahasiswa.pcr.ac.id>
2025-10-23 10:46:22 +0700

    membuat partial view

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -35,54 +35,36 @@
         <div class="col-12 mb-4">
             <div class="card border-0 shadow components-section">
                 <div class="card-body">
-                    <form action="{{ route('pelanggan.store') }}" method="POST">
+                    <form action="{{ route('user.store') }}" method="POST">
                         @csrf
                         <div class="row mb-4">
                             <div class="col-lg-4 col-sm-6">
-                                <!-- First Name -->
+                                <!-- Name -->
                                 <div class="mb-3">
-                                    <label for="first_name" class="form-label">First name</label>
-                                    <input type="text" name="first_name" id="first_name" class="form-control" required>
+                                    <label for="name" class="form-label"> name</label>
+                                    <input type="text" name="name" id="name" class="form-control" required>
                                 </div>
 
-                                <!-- Last Name -->
-                                <div class="mb-3">
-                                    <label for="last_name" class="form-label">Last name</label>
-                                    <input type="text" name="last_name" id="last_name" class="form-control" required>
-                                </div>
-                            </div>
 
-                            <div class="col-lg-4 col-sm-6">
-                                <!-- Birthday -->
-                                <div class="mb-3">
-                                    <label for="birthday" class="form-label">Birthday</label>
-                                    <input type="date" name="birthday" id="birthday" class="form-control">
-                                </div>
-
-                                <!-- Gender -->
-                                <div class="mb-3">
-                                    <label for="gender" class="form-label">Gender</label>
-                                    <select id="gender" name="gender" class="form-select">
-                                        <option value="">-- Pilih --</option>
-                                        <option value="Male">Male</option>
-                                        <option value="Female">Female</option>
-                                        <option value="Other">Other</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4 col-sm-12">
                                 <!-- Email -->
                                 <div class="mb-3">
                                     <label for="email" class="form-label">Email</label>
                                     <input type="text" name="email" id="email" class="form-control" required>
                                 </div>
-
-                                <!-- Phone -->
+                            </div>
+                            <div class="col-lg-4 col-sm-6">
+                                <!-- Password -->
                                 <div class="mb-3">
-                                    <label for="phone" class="form-label">Phone</label>
-                                    <input type="text" name="phone" id="phone" class="form-control">
+                                    <label for="Password" class="form-label">Password</label>
+                                    <input type="Password" name="Password" id="Password" class="form-control">
                                 </div>
+
+                                <!-- Password confirmation -->
+                                <div class="mb-3">
+                                    <label for="Password confirmation" class="form-label">Password confirmation</label>
+                                    <input type="Password" name="Password confirmation" id="Password confirmation" class="form-control">
+                                </div>
+                            </div>
 
                                 <!-- Buttons -->
                                 <div class="">
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
-use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserController;
 
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PelangganController;
@@ -44,3 +45,5 @@ Route::get('dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
 Route::resource('pelanggan', PelangganController::class);
+
+route::resource('user', UserController::class);
